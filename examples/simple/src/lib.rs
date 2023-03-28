fba5a5865387b7b9497dd6b7bc519150f65865c4
Brian H <brian.hardock@fermyon.com>
2023-03-28 16:17:41 -0400

    Add router prototype and example app
    Signed-off-by: Brian H <brian.hardock@fermyon.com>

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,15 +1,36 @@
-use anyhow::Result;
-use spin_sdk::{
-    http::{Request, Response},
-    http_component,
-};
+use spin_sdk::{http_component, http::{Request, Response}};
 
-/// A simple Spin HTTP component.
 #[http_component]
-fn handle_simple(req: Request) -> Result<Response> {
-    println!("{:?}", req.headers());
-    Ok(http::Response::builder()
-        .status(200)
-        .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+fn handle_simple(req: Request) -> anyhow::Result<Response> {
+    (spin_sdk_router::router! {
+        GET "/hello/:planet" => api::h1,
+        GET "/*" => api::h2,
+        GET "/nested/*" => api::h3,
+        GET "/hello/earth" => api::h4,
+        GET "/" => |ctx| { todo!("WHAT") }
+    })(req)
+}
+
+mod api {
+    use spin_sdk_router::{Context, Response};
+
+    // /hello/:planet
+    pub fn h1(Context { request: _, params }: Context) -> anyhow::Result<Response> {
+        let planet = params.get("planet").expect("PLANET");
+        println!("{planet}");
+
+        Ok(http::Response::builder()
+            .status(http::StatusCode::OK)
+            .body(None)
+            .unwrap())
+    }
+
+    // /*
+    pub fn h2(_ctx: Context) -> anyhow::Result<Response> { todo!("h2") }
+    
+    // /nested/*
+    pub fn h3(_ctx: Context) -> anyhow::Result<Response> { todo!("h3") }
+    
+    // /hello/earth
+    pub fn h4(_ctx: Context) -> anyhow::Result<Response> { todo!("h4") }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,120 @@
-// TODO
+use routefinder::{Captures, Router as MethodRouter};
+use std::collections::HashMap;
+
+type Handler = dyn Fn(Context) -> anyhow::Result<Response>;
+
+pub type Response = http::Response<Option<bytes::Bytes>>;
+pub type Request = http::Request<Option<bytes::Bytes>>;
+
+pub type Params = Captures<'static, 'static>;
+
+pub struct Context {
+    pub request: Request,
+    pub params: Params,
+}
+
+pub struct Router {
+    method_map: HashMap<http::Method, MethodRouter<Box<Handler>>>,
+    all_method_router: MethodRouter<Box<Handler>>,
+}
+
+struct Selection<'a> {
+    pub params: Captures<'static, 'static>,
+    pub handler: &'a Handler,
+}
+
+impl Router {
+    pub fn dispatch(&self, request: Request) -> anyhow::Result<Response> {
+        let method = request.method().to_owned();
+        let path = request.uri().path().to_owned();
+        let Selection { params, handler } = self.route(&path, method);
+        handler(Context { request, params })
+    }
+
+    fn route(&self, path: &str, method: http::Method) -> Selection<'_> {
+        if let Some(m) = self
+            .method_map
+            .get(&method)
+            .and_then(|r| r.best_match(path))
+        {
+            Selection {
+                handler: m.handler(),
+                params: m.captures().into_owned(),
+            }
+        } else if let Some(m) = self.all_method_router.best_match(path) {
+            Selection {
+                handler: m.handler(),
+                params: m.captures().into_owned(),
+            }
+        } else if method == http::Method::HEAD {
+            // If it is a HTTP HEAD request then check if there is a callback in the endpoints map
+            // if not then fallback to the behavior of HTTP GET else proceed as usual
+            self.route(path, http::Method::GET)
+        } else if self
+            .method_map
+            .iter()
+            .filter(|(k, _)| **k != method)
+            .any(|(_, r)| r.best_match(path).is_some())
+        {
+            // If this `path` can be handled by a callback registered with a different HTTP method
+            // should return 405 Method Not Allowed
+            Selection {
+                handler: &method_not_allowed,
+                params: Captures::default(),
+            }
+        } else {
+            Selection {
+                handler: &not_found,
+                params: Captures::default(),
+            }
+        }
+    }
+
+    pub fn add_all(&mut self, path: &str, handler: Box<Handler>) {
+        self.all_method_router.add(path, handler).unwrap();
+    }
+
+    pub fn add(&mut self, path: &str, method: http::Method, handler: Box<Handler>) {
+        self.method_map
+            .entry(method)
+            .or_insert_with(MethodRouter::new)
+            .add(path, handler)
+            .unwrap();   
+    }
+
+    pub fn new() -> Self {
+        Router {
+            method_map: HashMap::default(),
+            all_method_router: MethodRouter::new(),
+        }
+    }
+}
+
+fn not_found(_ctx: Context) -> anyhow::Result<Response> {
+    Ok(http::Response::builder()
+        .status(http::StatusCode::NOT_FOUND)
+        .body(None)
+        .unwrap())
+}
+
+fn method_not_allowed(_ctx: Context) -> anyhow::Result<Response> {
+    Ok(http::Response::builder()
+        .status(http::StatusCode::METHOD_NOT_ALLOWED)
+        .body(None)
+        .unwrap())
+}
+
+#[macro_export]
+macro_rules! router {
+    ($($method:ident $path:literal => $h:path),*) => {
+        {
+            let mut router = spin_sdk_router::Router::new();
+            $(
+                router.add($path, http::Method::$method, Box::new($h));
+            )*
+            move |req: Request| -> anyhow::Result<Response> {
+                router.dispatch(req)
+            }
+        }
+    };
+}
